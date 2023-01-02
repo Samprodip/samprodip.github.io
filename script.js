969f53a728fd57d2d2a6d900f45225b6a5bea930
Samprodip <91739905+Samprodip@users.noreply.github.com>
2023-01-02 22:30:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+/*playing algorithm*/
+const playlist = document.querySelector('#playlist');
+const player = document.querySelector('#player');
+const songName = document.querySelector('#song-name');
+
+playlist.addEventListener('click', event => {
+  const song = event.target;
+  const src = song.getAttribute('data-src');
+  player.src = src;
+  player.play();
+  songName.textContent = song.textContent;
+});
+
+
+/*player hide and show*/
+/*const playerr = document.querySelector('#player');
+const gifi = document.querySelector('#footer');
+
+    playerr.addEventListener('play', event => {
+      gifi.style.display = 'block';
+    });
+
+    playerr.addEventListener('pause', event => {
+      gifi.style.display = 'none';
+    });
+*/
+window.addEventListener("scroll", function(){
+var header = document.querySelector("header");
+header.classList.toggle("sticky", window.scrollY > 0);
+})
+
