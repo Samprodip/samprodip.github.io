0e2386179709b43e0a98483264fae9ec8e7acd20
Samprodip <91739905+Samprodip@users.noreply.github.com>
2023-03-06 23:41:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+/*Sticky header*/
+window.addEventListener("scroll", function(){
+var header = document.querySelector("header");
+header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+/*playing algorithm*/
+const playlist = document.querySelector('#playlist');
+const player = document.querySelector('#player');
+const songName = document.querySelector('#song-name');
+const pageTitle = document.querySelector('title');
+
+playlist.addEventListener('click', event => {
+  const currentSong = playlist.querySelector('.current-song');
+  currentSong.classList.remove('current-song');
+  const song = event.target;
+  song.classList.add('current-song');
+  const src = song.getAttribute('data-src');
+  player.src = src;
+  player.play();
+  songName.textContent = song.textContent;
+  pageTitle.textContent = song.textContent + " - Playing Now";
+});
+
+player.addEventListener('ended', () => {
+  const currentSong = playlist.querySelector('.current-song');
+  currentSong.classList.remove('current-song');
+  const nextSong = currentSong.nextElementSibling;
+  if (nextSong) {
+    nextSong.classList.add('current-song');
+    nextSong.click();
+  }
+});
