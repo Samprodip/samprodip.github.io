1f2197441a85a9100f0fd84d896e09fddcf320f7
Samprodip <91739905+Samprodip@users.noreply.github.com>
2023-03-07 19:25:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+/*playing algorithm*/
+const playlist = document.querySelector('#playlist');
+const player = document.querySelector('#player');
+const songName = document.querySelector('#song-name');
+const pageTitle = document.querySelector('title');
+
+let currentSongIndex = 0;
+let isPlaying = false;
+
+function playSong(songIndex) {
+  const song = playlist.querySelectorAll('.song')[songIndex];
+  const src = song.getAttribute('data-src');
+  
+  songName.textContent = song.textContent;
+  pageTitle.textContent = song.textContent + " - Playing Now";
+  player.src = src;
+  player.play();
+
+  playlist.querySelectorAll('.song').forEach((song, index) => {
+    if (index === songIndex) {
+      song.classList.add('current-song');
+    } else {
+      song.classList.remove('current-song');
+    }
+  });
+
+  currentSongIndex = songIndex;
+  isPlaying = true;
+}
+
+playlist.addEventListener('click', event => {
+  const song = event.target.closest('.song');
+  if (song) {
+    const songIndex = Array.from(playlist.querySelectorAll('.song')).indexOf(song);
+    playSong(songIndex);
+  }
+});
+
+player.addEventListener('ended', () => {
+  const nextSongIndex = (currentSongIndex + 1) % playlist.querySelectorAll('.song').length;
+  playSong(nextSongIndex);
+});
+
+/*Sticky Header*/
+window.addEventListener("scroll", function(){
+var header = document.querySelector("header");
+header.classList.toggle("sticky", window.scrollY > 0);
+});
