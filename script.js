dd16b017ee065967cbfa09d8a23709bd46703912
Samprodip <91739905+Samprodip@users.noreply.github.com>
2023-03-07 11:15:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
-/*Sticky header*/
-window.addEventListener("scroll", function(){
-var header = document.querySelector("header");
-header.classList.toggle("sticky", window.scrollY > 0);
-})
-
 /*playing algorithm*/
 const playlist = document.querySelector('#playlist');
 const player = document.querySelector('#player');
 const songName = document.querySelector('#song-name');
 const pageTitle = document.querySelector('title');
 
-playlist.addEventListener('click', event => {
-  const currentSong = playlist.querySelector('.current-song');
-  currentSong.classList.remove('current-song');
-  const song = event.target;
-  song.classList.add('current-song');
+let currentSongIndex = 0;
+let isPlaying = false;
+
+function playSong(songIndex) {
+  const song = playlist.querySelectorAll('.song')[songIndex];
   const src = song.getAttribute('data-src');
-  player.src = src;
-  player.play();
+  
   songName.textContent = song.textContent;
   pageTitle.textContent = song.textContent + " - Playing Now";
+  player.src = src;
+  player.play();
+
+  playlist.querySelectorAll('.song').forEach((song, index) => {
+    if (index === songIndex) {
+      song.classList.add('current-song');
+    } else {
+      song.classList.remove('current-song');
+    }
+  });
+
+  currentSongIndex = songIndex;
+  isPlaying = true;
+}
+
+playlist.addEventListener('click', event => {
+  const song = event.target.closest('.song');
+  if (song) {
+    const songIndex = Array.from(playlist.querySelectorAll('.song')).indexOf(song);
+    playSong(songIndex);
+  }
 });
 
 player.addEventListener('ended', () => {
-  const currentSong = playlist.querySelector('.current-song');
-  currentSong.classList.remove('current-song');
-  const nextSong = currentSong.nextElementSibling;
-  if (nextSong) {
-    nextSong.classList.add('current-song');
-    nextSong.click();
-  }
+  const nextSongIndex = (currentSongIndex + 1) % playlist.querySelectorAll('.song').length;
+  playSong(nextSongIndex);
+});
+
+/*Sticky Header*/
+window.addEventListener("scroll", function(){
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
