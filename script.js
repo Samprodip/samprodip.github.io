63c8474cbb44bacb4b89442a0b16e5e7ab6a5800
Samprodip <91739905+Samprodip@users.noreply.github.com>
2023-03-09 18:23:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 /*playing algorithm*/
+/*playing algorithm*/
 const playlist = document.querySelector('#playlist');
 const player = document.querySelector('#player');
 const songName = document.querySelector('#song-name');
 const pageTitle = document.querySelector('title');
 
-let currentSongIndex = 0;
+let currentSongIndex = localStorage.getItem('currentSongIndex') || 0;
 let isPlaying = false;
 
 function playSong(songIndex) {
@@ -14,6 +15,7 @@ function playSong(songIndex) {
   songName.textContent = song.textContent;
   pageTitle.textContent = song.textContent + " - Playing Now";
   player.src = src;
+  player.currentTime = localStorage.getItem('currentTime') || 0; // resume from the stored time position
   player.play();
 
   playlist.querySelectorAll('.song').forEach((song, index) => {
@@ -26,6 +28,10 @@ function playSong(songIndex) {
 
   currentSongIndex = songIndex;
   isPlaying = true;
+
+  // store the current song index and time position in localStorage
+  localStorage.setItem('currentSongIndex', currentSongIndex);
+  localStorage.setItem('currentTime', player.currentTime);
 }
 
 playlist.addEventListener('click', event => {
@@ -40,6 +46,18 @@ player.addEventListener('ended', () => {
   const nextSongIndex = (currentSongIndex + 1) % playlist.querySelectorAll('.song').length;
   playSong(nextSongIndex);
 });
+
+// check if there is a stored song index and resume playing the song
+if (localStorage.getItem('currentSongIndex')) {
+  playSong(currentSongIndex);
+}
+
+/*Sticky Header*/
+window.addEventListener("scroll", function(){
+var header = document.querySelector("header");
+header.classList.toggle("sticky", window.scrollY > 0);
+}); 
+
 
 /*Sticky Header*/
 window.addEventListener("scroll", function(){
